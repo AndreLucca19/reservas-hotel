6f492910eee97733d690c3c5e3bdc328264399a1
André Lucca <157771933+AndreLucca19@users.noreply.github.com>
2024-11-05 10:44:49 -0300

    finsh

--- andre_lucca_sql.sql
+++ andre_lucca_sql.sql
@@ -27,25 +27,41 @@ CONSTRAINT fk_quartos FOREIGN KEY (id_quarto) REFERENCES quartos (id_quarto)
 INSERT INTO hospedes (nome, email) 
 VALUES 
 ('Luiz', 'luiz@gmail.com'),
-('Andre', 'andre@gmail.com');
+('Pedro', 'pedro@gmail.com'),
+('Joao', 'joao@gmail.com'),
+('Andre', 'andre@gmail.com'),
+('Caio', 'caio@gmail.com'),
+('Lucca', 'lucca@gmail.com'),
+('Luana', 'luana@gmail.com');
+
 
 INSERT INTO quartos (numero_quarto, camas) 
 VALUES
 (186, 2),
 (193, 1),
 (134, 2),
-(172, 4);
+(172, 4),
+(191, 2);
+(192, 4),
+(194, 2);
 
-INSERT INTO reservas (id_hospede, id_quarto, horario_reserva) VALUES 
-(1,3, '00:20'),
-(2,4, '01:10');
+INSERT INTO reservas (id_hospede, id_quarto, horario_reserva, data_inicio, data_fim) 
+VALUES 
+(1, 1, '01:20', '2024-01-01', '2024-01-04'),
+(2, 2, '01:00', '2024-12-25', '2025-01-03'),
+(3, 5, '09:30', '2024-01-01', '2024-01-04'),
+(5, 3, '12:00', '2024-02-16', '2024-02-19'),
+(4, 4, '10:00', '2024-06-01', '2024-06-09'),
 
+/* Pega todos os reservados*/
 SELECT 
 	r.id_reserva,
 	h.nome AS hospede,
 	h.email,
 	q.numero_quarto,
 	q.camas AS quantidade_cama,
+    r.data_inicio,
+    r.data_fim,
 	r.data_reserva,
 	r.horario_reserva
 FROM	
@@ -54,3 +70,41 @@ JOIN
 	hospedes h ON r.id_hospede = h.id_hospede
 JOIN
 	quartos q ON r.id_quarto = q.id_quarto;
+
+/*mostra todos os hóspedes, incluindo aqueles que ainda não estão reservados*/
+SELECT 
+        r.id_reserva,
+        h.nome AS hospede,
+        h.email,
+        q.numero_quarto,
+        q.camas AS quantidade_cama,
+        r.data_inicio,
+        r.data_fim,
+        r.data_reserva,
+        r.horario_reserva
+FROM	
+        reservas r
+LEFT JOIN
+        quartos q ON r.id_quarto = q.id_quarto
+RIGHT JOIN
+        hospedes h ON r.id_hospede = h.id_hospede;
+
+/*mostra apenas os quartos que ainda não estão reservados*/
+SELECT 
+        r.id_reserva,
+        h.nome AS hospede,
+        h.email,
+        q.numero_quarto,
+        q.camas AS quantidade_cama,
+        r.data_inicio,
+        r.data_fim,
+        r.data_reserva,
+        r.horario_reserva
+FROM	
+    quartos q
+LEFT JOIN
+    reservas r ON q.id_quarto = r.id_quarto
+LEFT JOIN
+    hospedes h ON r.id_hospede = h.id_hospede
+WHERE
+    r.id_reserva IS NULL;
